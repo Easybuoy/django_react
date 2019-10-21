4a8f6b8b2b64a5397cc52284e88ab0a7b1b8394b
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-21 09:28:31 +0100

    setup model

---
+++ leadmanager/leads/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email= models.EmailField(max_length=100, unique=True)
+    message= models.CharField(max_length=500, blank=True)
