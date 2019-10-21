c80bf27de5df2cd67d67998a496aeb37bab7475e
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-21 09:50:59 +0100

    setup routing

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Lead(models.Model):
     name = models.CharField(max_length=100)
-    email= models.EmailField(max_length=100, unique=True)
-    message= models.CharField(max_length=500, blank=True)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateField(auto_now_add=True)
